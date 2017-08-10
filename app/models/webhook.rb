be62586e1e0f46f83c802a13ea04452c5ce9ba44
Edmore Moyo <edmore@samewave.com>
2017-08-10 14:07:34 +0200

    Refactoring

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -2,11 +2,11 @@ class Webhook < ApplicationRecord
   after_create :process_webhook
 
   # def is_github?
-  #   integration_name == 'github'
+  #   integration_id == 'github'
   # end
   #
   # def is_pivotal?
-  #   integration_name == 'pivotal-tracker'
+  #   integration_id == 'pivotal-tracker'
   # end
 
   private
--- app/services/Integrations/github.rb
+++ app/services/Integrations/github.rb
@@ -1,6 +1,6 @@
 module Integrations
   class Github < Base
-    def self.process(integration_object)
+    def self.process(integration)
       # 1. Get the fields necessary to construct comment and format accordingly
       # 2. create comment
     end
